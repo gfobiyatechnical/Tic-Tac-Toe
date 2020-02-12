b82e044d59517f5a28828a07fdb952a686f68c16
Rakesh Yadav <rakesh.yadav_cs18@gla.ac.in>
2020-02-12 01:45:00 -0800

    commit
    Signed-off-by: RakeshY008 <rakesh.yadav_cs18@gla.ac.in>

--- Problems/The product of numbers from a to b/src/Main.java
+++ Problems/The product of numbers from a to b/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        long product = 1;
+        if (num1 < num2) {
+            for (int i = num1; i < num2; i++) {
+                product = product * i;
+
+            }
+            System.out.println(product);
+        }
     }
 }
